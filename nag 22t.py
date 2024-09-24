99263f55d1d96c1b502c1777a424c5036e3491cc
Naga9392 <155941373+Naga9392@users.noreply.github.com>
2024-09-24 14:36:26 +0530

    Add files via upload

---
+++ nag 22t.py
@@ -0,0 +1,4 @@
+for x in range(2,10):
+  b=1/x
+  print(b)
+  
---
+++ p12.py
@@ -0,0 +1,8 @@
+a=int(input("enter user defined number"))
+def countdown(n):
+    while(n>=0):
+        print(n)
+        n=n-1
+countdown(a)
+
+            
